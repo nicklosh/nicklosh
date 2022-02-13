55b2d8d138ad035d755113483f0d71506c8efc81
Nick Long <nick.long@sentral.com.aui>
2022-02-14 08:51:05 +1100

    Replace depricated import with use in scss files and move home page to seperate files in preperation for additional pages

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
 import './styles/App.scss';
 import React from 'react';
-import LinkMenu from './LinkMenu';
-import Bull from './images/Bull';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
-      <div className="container">
-
-        <div className="layer overflow">
-          <div className="layer">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_90">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_180">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_270">
-            <div className="triangle"></div>
-          </div>
-        </div>
-
-        <div className="layer spin overflow">
-          <div className="layer">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_90">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_180">
-            <div className="triangle"></div>
-          </div>
-          <div className="layer rotate_270">
-            <div className="triangle"></div>
-          </div>
-        </div>
-
-        <div className="layer overflow">
-          <div className="hexagon_shadow">
-            <div className="hexagon"></div>          
-          </div>
-        </div>
-      
-        <div className="layer">
-          <Bull className="bull"/>
-        </div>
-
-      </div>
-      <LinkMenu />
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,55 @@
+import '../styles/Home.scss';
+import React from 'react';
+import LinkMenu from '../LinkMenu';
+import Bull from '../images/Bull';
+
+export default function Home() {
+  return (
+    <div>
+    <div className="container">
+
+      <div className="layer overflow">
+        <div className="layer">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_90">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_180">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_270">
+          <div className="triangle"></div>
+        </div>
+      </div>
+
+      <div className="layer spin overflow">
+        <div className="layer">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_90">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_180">
+          <div className="triangle"></div>
+        </div>
+        <div className="layer rotate_270">
+          <div className="triangle"></div>
+        </div>
+      </div>
+
+      <div className="layer overflow">
+        <div className="hexagon_shadow">
+          <div className="hexagon"></div>          
+        </div>
+      </div>
+    
+      <div className="layer">
+        <Bull className="bull"/>
+      </div>
+
+    </div>
+    <LinkMenu />
+  </div>
+  )
+}
